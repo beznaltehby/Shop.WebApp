7c896056794a82d4ff2ccc7e8bc1f66c065d8a3b
Evgeny Makhnovets <evgeny.makhnovets@gmail.com>
2017-04-11 00:58:48 +0300

    Fix base url for api

--- src/app/core/services/constants.js
+++ src/app/core/services/constants.js
@@ -3,7 +3,8 @@
 export default function (app) {
     app
         .constant('consts', {
-            baseApiUrl: 'http://localhost:3000/api',
+            //baseApiUrl: 'http://localhost:3000/api', // TODO : uncomment for dev mode
+            baseApiUrl: '/api',
             logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/No-logo.svg/2000px-No-logo.svg.png'
         });
 }
