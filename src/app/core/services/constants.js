fa4a12ab23c9fc26752ab7b31702b11cf92336a2
Evgeny Makhnovets <evgeny.makhnovets@gmail.com>
2017-04-15 02:03:49 +0300

    Revert base url

--- src/app/core/services/constants.js
+++ src/app/core/services/constants.js
@@ -3,8 +3,8 @@
 export default function (app) {
     app
         .constant('consts', {
-            baseApiUrl: 'http://raccoondev.tk/api', // TODO : uncomment for dev mode
-            //baseApiUrl: '/api',
+            //baseApiUrl: 'http://raccoondev.tk/api', // TODO : uncomment for dev mode
+            baseApiUrl: '/api',
             logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/No-logo.svg/2000px-No-logo.svg.png'
         });
 }
