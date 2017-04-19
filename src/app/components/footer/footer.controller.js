ba0ce74fcf5214dc026aedce60b805d8ec10cc8f
Evgeny Makhnovets <evgeny.makhnovets@gmail.com>
2017-04-19 23:18:23 +0300

    Added brands logos

--- src/app/components/footer/footer.controller.js
+++ src/app/components/footer/footer.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 import moment from 'moment';
+import acerLogo from '_images/brands/acer-logo.png';
+import apcLogo from '_images/brands/apc-logo.png';
+import appleLogo from '_images/brands/apple-logo.png';
+import asusLogo from '_images/brands/asus-logo.png';
+import canonLogo from '_images/brands/canon-logo.png';
+import ciscoLogo from '_images/brands/cisco-logo.png';
+import hpLogo from '_images/brands/hp-logo.png';
+import lenovoLogo from '_images/brands/lenovo-logo.png';
+import logitechLogo from '_images/brands/logitech-logo.png';
+import microsoftLogo from '_images/brands/microsoft-logo.png';
 
 function footerController($rootScope) {
     'ngInject';
@@ -14,6 +24,18 @@ function footerController($rootScope) {
     });
 
     vm.year = moment().year();
+    vm.brandsLogos = [
+        canonLogo,
+        hpLogo,
+        asusLogo,
+        appleLogo,
+        ciscoLogo,
+        microsoftLogo,
+        apcLogo,
+        acerLogo,
+        lenovoLogo,
+        logitechLogo
+    ];
 }
 
 export default footerController;
